5840faf0d7f28dbca996dd038eae4ccdfa7fb9a6
Przemek <pzawadzki95@gmail.com>
2016-03-19 00:26:17 +0100

    no message

--- bank-client/src/main/java/bank/example/util/ConnectionFactory.java
+++ bank-client/src/main/java/bank/example/util/ConnectionFactory.java
@@ -5,10 +5,38 @@
  */
 package bank.example.util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  *
  * @author Przemek DELL
  */
 public class ConnectionFactory {
+     private ClientDataSource ds;
+
+    private ConnectionFactory() {
+        ds = new ClientDataSource();
+        ds.setDatabaseName("bankDB");
+        ds.setUser("Zawadzki");
+        ds.setPassword("password");
+        ds.setPortNumber(1527);
+        ds.setServerName("localhost");
+    }
     
+    private static class SingletonHolder {
+        private static final ConnectionFactory INSTANCE = new ConnectionFactory();
+    }
+
+    public static ConnectionFactory getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+    public static Connection getConnection() {
+        try {
+            return getInstance().ds.getConnection();
+        } catch (SQLException ex) {
+            throw new DataAccessException(ex);
+        }
+    }
 }
